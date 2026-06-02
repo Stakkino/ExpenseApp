f792f36527678c5729c6ecef6a2b366956082212
Stakkino <jhenstakkino@gmail.com>
2026-06-02 17:21:46 +0300

    structure initiale du projet

---
+++ config.exemple.py
@@ -0,0 +1,7 @@
+# config.exemple.py  ← commitina @ GitHub
+DB_CONFIG = {
+    "host":     "localhost",
+    "user":     "ton_user_mysql",
+    "password": "ton_mot_de_passe",
+    "database": "depense_db"
+}
